9ab8de361035ae0c8e02908b8b088f02420f5767
Isabela Siqueira <isabela_siq@outlook.com>
2022-05-14 00:10:44 -0300

    Adicionando arquivos base

---
+++ WebAPI-Case/Controllers/UsuarioController.cs
@@ -0,0 +1,81 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+namespace WebAPI_Case.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsuarioController : ControllerBase
+    {
+        //TODO
+        //Implementar camada de servico
+
+        /// <summary>
+        /// Este endpoint deve consultar as interessadas cadastradas
+        /// </summary>
+        /// <returns>
+        /// Retorna a lista com todas as interessadas cadastradas
+        /// </returns>
+        [HttpGet]
+        public IActionResult ConsultarTodos()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Este endpoint deve consultar o cadastro através do nome e e-mail
+        /// </summary>
+        /// <returns>
+        /// Retorna 2xx caso sucesso
+        /// Retorna 4xx caso erro
+        /// </returns>
+        [HttpGet]
+        [Route("consultar/{nome}/{email}")]
+        public IActionResult ConsultarPorEmail(string nome, string email)
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        ///  Este endpoint deve realizar a inclusao
+        /// </summary>
+        /// <returns>
+        /// Retorna 2xx caso sucesso
+        /// Retorna 4xx caso erro
+        /// </returns>
+        [HttpPost]
+        [Route("incluir")]
+        public IActionResult Adicionar()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Este endpoint deve atualizar os dados cadastrados
+        /// </summary>
+        /// <returns>
+        /// Retorna 2xx caso sucesso
+        /// Retorna 4xx caso erro
+        /// </returns>
+        [HttpPut]
+        [Route("atualizar")]
+        public IActionResult Atualizar()
+        {
+            return Ok();
+        }
+
+        /// <summary>
+        /// Este endpoint deve excluir um cadastro através do e-mail informado
+        /// </summary>
+        /// <returns>
+        /// Retorna 2xx caso sucesso
+        /// Retorna 4xx caso erro
+        /// </returns>
+        [HttpDelete]
+        [Route("excluir/{email}")]
+        public IActionResult Excluir(string email)
+        {
+            return Ok();
+        }
+    }
+}
